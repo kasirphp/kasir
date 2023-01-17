6d61ca57811584a9a07c6af53fc7026280fa7f23
Risang Baskoro <risangbaskoro@gmail.com>
2023-01-17 20:27:26 +0700

    remove type int from order_id

--- src/Concerns/HasOrderId.php
+++ src/Concerns/HasOrderId.php
@@ -4,16 +4,16 @@ namespace Kasir\Kasir\Concerns;
 
 trait HasOrderId
 {
-    protected string|int $order_id;
+    protected string $order_id;
 
-    public function orderId(string|int $order_id): static
+    public function orderId(string $order_id): static
     {
         $this->transaction_details['order_id'] = $order_id;
 
         return $this;
     }
 
-    public function getOrderId(): string|int
+    public function getOrderId(): string
     {
         return $this->evaluate($this->transaction_details['order_id']);
     }

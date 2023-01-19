ee41db197b569e9c6a302558135c2b9ae98bb3f9
Risang Baskoro <risangbaskoro@gmail.com>
2023-01-19 20:22:47 +0700

    fix requestor client/server key test

--- tests/Feature/RequestorTest.php
+++ tests/Feature/RequestorTest.php
@@ -3,6 +3,19 @@
 use Kasir\Kasir\Exceptions\MidtransKeyException;
 use Kasir\Kasir\Helper\Requestor;
 
-it('throws MidtransKeyException when the server/client key is not defined', function () {
-    Requestor::call('http://example.com', null, [], 'POST');
-})->throws(MidtransKeyException::class);
+it('throws MidtransKeyException when validating client/server key', function ($server_key, $message) {
+    expect(fn () => Requestor::call('http://example.com', $server_key, [], 'POST'))->toThrow(MidtransKeyException::class, $message);
+})->with([
+    'null' => [
+        null,
+        'The Server/Client Key is null. Please double check the config or env file.',
+    ],
+    'empty string' => [
+        '',
+        'The Server/Client Key is empty. Please double check the config or env file.',
+    ],
+    'contains whitespace' => [
+        'foo bar',
+        'The Server/Client Key contains a whitespace character. Please double check the config or env file.',
+    ],
+]);

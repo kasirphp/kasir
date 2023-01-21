62cfa679acffc56306df651fabb92631a891e1ac
Risang Baskoro <risangbaskoro@gmail.com>
2023-01-21 12:41:06 +0700

    Create Notification Test in HttpTest.php

--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -20,3 +20,28 @@ it('throws MidtransKeyException when validating client/server key', function ($s
         'The Server/Client Key contains a whitespace character. Please double check the config or env file.',
     ],
 ]);
+
+it('creates notification header if exists', function ($urls) {
+    Config::set('kasir.notification_url.append', $urls);
+    Config::set('kasir.notification_url.override', $urls);
+
+    $request = Http::request(config('kasir.server_key'), []);
+    $headers = $request->getOptions()['headers'];
+
+    $header_expectation = implode(',', array_slice($urls, 0, 3));
+
+    if (! $urls) {
+        expect($headers)->not->toHaveKeys([
+            'X-Append-Notification',
+            'X-Override-Notification',
+        ]);
+    } else {
+        expect($headers['X-Override-Notification'])->toBeString()->toBe($header_expectation)
+            ->and($headers['X-Append-Notification'])->toBeString()->toBe($header_expectation);
+    }
+})->with([
+    'empty notification' => [[]],
+    'one url' => [['https://google.com']],
+    'three urls' => [['https://google.com', 'https://twitter.com', 'https://github.com']],
+    'four urls' => [['https://google.com', 'https://twitter.com', 'https://github.com', 'https://example.com']],
+]);

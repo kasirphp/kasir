d29fa2729c72596b552c179ae55c235cf415fdfe
Risang Baskoro <risangbaskoro@gmail.com>
2023-01-24 16:24:39 +0700

    MidtransResponse now have actions() method to get array of actions

--- src/Helper/MidtransResponse.php
+++ src/Helper/MidtransResponse.php
@@ -98,6 +98,47 @@ class MidtransResponse implements ArrayAccess
     }
 
     /**
+     * Get response actions.
+     *
+     * @return mixed
+     */
+    public function actions(): mixed
+    {
+        return $this->json('actions');
+    }
+
+    /**
+     * Get response action names.
+     *
+     * @return array|null
+     */
+    public function actionsName(): array | null
+    {
+        return $this->actions() ? array_column($this->actions(), 'name') : null;
+    }
+
+    /**
+     * Get response action by name.
+     *
+     * @param $name
+     * @return mixed|null
+     */
+    public function action($name): mixed
+    {
+        if (! $this->actions()) {
+            return null;
+        }
+
+        $array = array_filter($this->actions(), function ($action) use ($name) {
+            return $action['name'] === $name;
+        });
+
+        $result = call_user_func_array('array_merge', $array);
+
+        return ! empty($result) ? $result : null;
+    }
+
+    /**
      * Get a header from the response.
      *
      * @param  string  $header

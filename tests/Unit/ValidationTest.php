cc581f347d3eb7bf821d9093ea09c458e2ef340b
Risang Baskoro <risangbaskoro@gmail.com>
2023-02-10 06:03:13 +0700

    automatically calculates gross_amount if itemDetails() method is called

--- tests/Unit/ValidationTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-it('throws correct validation Exceptions', function ($kasir, $exception) {
-    expect(fn () => $kasir->validate())->toThrow($exception);
-})->with('validation_exceptions');

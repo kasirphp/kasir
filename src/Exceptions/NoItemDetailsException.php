824e2b1109aaf66d938f0c8797cf4aceb2ac9881
Risang Baskoro <risangbaskoro@gmail.com>
2023-02-10 06:17:50 +0700

    fix NoItemDetailsException message

--- src/Exceptions/NoItemDetailsException.php
+++ src/Exceptions/NoItemDetailsException.php
@@ -5,7 +5,7 @@ namespace Kasir\Kasir\Exceptions;
 class NoItemDetailsException extends \Exception
 {
     public function __construct(
-        string $message = 'if gross_amount is not set, item_details must not null',
+        string $message = 'if gross_amount is not set, item_details must not be empty',
         int $code = 403,
         ?\Throwable $previous = null
     ) {

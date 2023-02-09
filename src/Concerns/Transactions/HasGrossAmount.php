05891e86c5b095274269a2168ecf5868638f3d05
Risang Baskoro <risangbaskoro@gmail.com>
2023-02-10 04:48:32 +0700

    update type decalrations

--- src/Concerns/Transactions/HasGrossAmount.php
+++ src/Concerns/Transactions/HasGrossAmount.php
@@ -4,16 +4,16 @@ namespace Kasir\Kasir\Concerns\Transactions;
 
 trait HasGrossAmount
 {
-    protected ?int $gross_amount;
+    protected int | null $gross_amount;
 
-    public function grossAmount(?int $gross_amount): static
+    public function grossAmount(int | null $gross_amount): static
     {
         $this->transaction_details['gross_amount'] = $this->evaluate($gross_amount);
 
         return $this;
     }
 
-    public function getGrossAmount(): ?int
+    public function getGrossAmount(): int | null
     {
         return $this->evaluate($this->transaction_details)['gross_amount'];
     }

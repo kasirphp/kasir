ee346b48c75f5a992d3d1787792cf12aa1d220b6
Risang Baskoro <risangbaskoro@gmail.com>
2023-02-23 00:27:28 +0700

    Parameter `account_id` on GoPay payment method accepts GoPay instance.
    It will automatically get the accountId() if available, and check for `status` from Midtrans if `accountId` in the object is not present.

--- src/Payment/EMoney/GoPay.php
+++ src/Payment/EMoney/GoPay.php
@@ -12,7 +12,7 @@ class GoPay extends PaymentObject implements PaymentMethod
      *
      * @param  bool  $enable_callback  Required for GoPay deeplink/QRIS. To determine appending callback URL in the deeplink. Default value: false.
      * @param  string|null  $callback_url  The HTTP or Deeplink URL to which the customer is redirected from Gojek app after successful payment. Default value: callback_url in dashboard settings.
-     * @param  string|null  $account_id  Required for GoPay Tokenization. The customer account ID linked during Create Pay Account API.
+     * @param  string|\Kasir\Kasir\GoPay|null  $account_id  Required for GoPay Tokenization. The customer account ID linked during Create Pay Account API.
      * @param  string|null  $payment_option_token  Required for GoPay Tokenization. Token to specify the payment option made by the customer from Get Pay Account API metadata.
      * @param  bool  $pre_auth  Set the value to true to reserve the specified amount from the customer balance. Once the customer balance is reserved, you can initiate a subsequent Capture API request. Default value: false.
      * @param  bool  $recurring  Set the value to true to mark as a recurring transaction, only allowed for authorised merchant. Default value: false
@@ -23,12 +23,21 @@ class GoPay extends PaymentObject implements PaymentMethod
     public static function make(
         bool $enable_callback = false,
         string | null $callback_url = null,
-        string | null $account_id = null,
+        string | \Kasir\Kasir\GoPay | null $account_id = null,
         string | null $payment_option_token = null,
         bool $pre_auth = false,
         bool $recurring = false
     ): static {
         $options = array_filter(get_defined_vars(), 'strlen');
+
+        if ($account_id instanceof \Kasir\Kasir\GoPay) {
+            if (!$account_id->accountId())
+            {
+                $account_id->status();
+            }
+            $options['account_id'] = $account_id->accountId();
+        }
+
         $key = 'gopay';
 
         return app(static::class)
